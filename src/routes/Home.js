b7a0cfdfdf80334899df971ca9a070cad3ea4b3d
Hedinn Eiriksson <hedinne@gmail.com>
2016-09-28 21:32:12 +0000

    Test failed.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import Segment from 'components/segment';
-import InstagramLogin from 'react-instagram-login';
-
-const responseInstagram = (response) => {
-  console.log(response);
-};
 
 /**
  * Home route component
@@ -22,12 +17,6 @@ export default class Home extends Component {
         <Helmet title="Home" />
         <Segment>
           <h1>Woop Woop</h1>
-          <InstagramLogin
-            clientId="cf5856ebccbb459aaba3e64adc37b54b"
-            buttonText="Login"
-            onSuccess={responseInstagram}
-            onFailure={responseInstagram}
-          />
         </Segment>
       </div>
     );
