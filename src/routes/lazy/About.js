a2ce21a84bc1e7d3b4694f88c17357a7d297b096
snaebjorn.valur <snaebjorn.valur@gmail.com>
2016-11-10 11:32:43 +0000

    Texta baett vid i about

--- src/routes/lazy/About.js
+++ src/routes/lazy/About.js
@@ -15,7 +15,18 @@ export default class About extends Component {
       <div>
         <Helmet title="About" />
         <Segment>
-          <h1>About</h1>
+          <h1>Emotional intelligence verkefnið</h1>
+          <p> Verkefnið er unnið í áfanganum hugbúnaðarþróun við Háskóla Íslands undir kennslu Matthias Book. </p>
+          <p>
+              Emotional intelligence byggir í grunninn á Express og React en nýtir að auki utanaðkomandi API þjónustur.
+              Helst má þar nefna Instagram API og Microsoft Cognitive Services betur þekkt sem Face API.
+              Myndum instagram notanda er aflað í gegnum Instagram API, þær greindar með Face API og niðurstöðum safnað saman.
+              Niðurstöður þessa ferlis eru tölulegar upplýsingar um tilfinningar sem andlitssvipir greindra mynda gefa til kynna.
+          </p>
+          <p>
+              Hugbúnaðurinn getur þessa stundina greint tilfinningar 15 seinustu instagram mynda notanda.
+              Einnig er hægt að bæta við öðrum notanda og bera saman niðurstöðurnar þessara tveggja notenda.
+          </p>
         </Segment>
       </div>
     );
