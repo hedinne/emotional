d38976a3c7d9d0b762ff8561b7dde6ae84eb6513
Gunnsteinn Aron Skúlason <gunnsteinnskula@gmail.com>
2016-11-22 16:34:14 +0000

    battlepage update

--- src/routes/Battle.js
+++ src/routes/Battle.js
@@ -4,6 +4,8 @@ import Segment from 'components/segment';
 import { observer } from 'mobx-react';
 import {Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis} from 'Recharts';
 import st from 'store/Basic';
+import s from 'routes/less/Player.less';
+import t from 'routes/less/Battle.less';
 
 
 @observer
@@ -16,10 +18,11 @@ export default class Battle extends Component {
 	render() {
 
 		return (
-				<div>
+				<div className={s.host}>
 				<Helmet title="Battle" />
 				<Segment>
-				<h2>Niðurstöður</h2>
+				<div className={s.container}>
+				<h2 className={s.heading}>Results</h2>
 				{/* <h4>
 						Það fundust {st.emo.count} andlit á seinustu 15 myndunum þínum.<br />
 						Þetta er algengi helstu tilfinninga á þeim myndum.
@@ -33,14 +36,29 @@ export default class Battle extends Component {
 						</div>
 						);
 						})} */}
-				<RadarChart cx={250} cy={200} width={480} height={400} data={st.cleanEmo}>
-				<Radar name={st.userInfoOne.full_name} dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
-				<Radar name={st.userInfoTwo.full_name} dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6}/>
-				<PolarGrid />
-				<Legend />
-				<PolarAngleAxis dataKey="subject" />
-				</RadarChart>
-				</Segment>
+
+					<div className={t.battlewindow}>
+						<div className={t.profile}>
+								<img src={st.userInfoOne.profile_picture}  className="t.image"/>
+						</div>
+						<div className={t.text}>
+								Vs.
+						</div>
+						<div className={t.profile}>
+								<img src={st.userInfoTwo.profile_picture} className="t.image"/>
+						</div>
+					</div>
+					<div className={t.chart}>
+								<RadarChart cx={250} cy={200} width={480} height={400} data={st.cleanEmo}>
+									<Radar name={st.userInfoOne.full_name} dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6}/>
+										<Radar name={st.userInfoTwo.full_name} dataKey="B" stroke="#82ca9d" fill="#82ca9d" fillOpacity={0.6}/>
+										<PolarGrid />
+									<Legend />
+									<PolarAngleAxis dataKey="subject" />
+								</RadarChart>
+							</div>
+						</div>
+					</Segment>
 				</div>
 				);
 	}
